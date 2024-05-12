9cb8525158af601c436c2c38db787faaeea47e80
Omar Adly <omaradly2020@gmail.com>
2024-05-12 14:49:16 +0300

    stash: index page test colors

--- apps/frontend/src/views/index.vue
+++ apps/frontend/src/views/index.vue
@@ -1,5 +1,11 @@
 <template>
     <div>
         <h1>starter page</h1>
+        <TheBreadcrumbs/>
+        <DataTable/>
     </div>
 </template>
+<script setup lang="ts">
+import TheBreadcrumbs from "@/components/ui/TheBreadcrumbs.vue";
+import DataTable from "@/components/ui/DataTable.vue";
+</script>
