e8a47bb1426428610f25e01c917572b9d160e4b4
abdelrahman-essawy <abdelrahman.mo.essawy@gmail.com>
2024-05-12 15:40:45 +0300

    fix: categories type

--- apps/backend/src/modules/brands/brands.controller.ts
+++ apps/backend/src/modules/brands/brands.controller.ts
@@ -51,14 +51,7 @@ import { saneOperationsId } from '../../common/swagger.config';
     exclude: ['replaceOneBase'],
   },
   serialize: {
-    getMany: SerializedBrandDto,
     get: SerializedBrandDto,
-    create: SerializedBrandDto,
-    replace: SerializedBrandDto,
-    recover: SerializedBrandDto,
-    delete: SerializedBrandDto,
-    createMany: SerializedBrandDto,
-    update: SerializedBrandDto,
   },
 })
 @Controller('brands')
