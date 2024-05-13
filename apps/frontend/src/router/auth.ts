fbd9a66cbe3d003c24db785ad6efe3934bee07c7
Omar Adly <omaradly2020@gmail.com>
2024-05-13 19:58:09 +0300

    feat: unlockScreen page

--- apps/frontend/src/router/auth.ts
+++ apps/frontend/src/router/auth.ts
@@ -18,4 +18,10 @@ export const authLinks :RouteRecordRaw[] = [
         component: () => import('@/pages/auth/passwordReset/TheIndex.vue'),
         meta: { layout: 'auth' },
     },
+    {
+        path: '/auth/lock-screen',
+        name: 'lock-screen',
+        component: () => import('@/pages/auth/LockScreen/TheIndex.vue'),
+        meta: { layout: 'auth' },
+    },
 ]
