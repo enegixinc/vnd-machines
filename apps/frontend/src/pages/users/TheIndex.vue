348085a72c32cccd32e25e9cf8261da92857f4b1
Omar Adly <omaradly2020@gmail.com>
2024-05-13 14:14:38 +0300

    refactor: user type from backend

--- apps/frontend/src/pages/users/TheIndex.vue
+++ apps/frontend/src/pages/users/TheIndex.vue
@@ -19,16 +19,19 @@ import type { Ref } from 'vue'
 import TheBreadcrumbs from "@/components/ui/TheBreadcrumbs.vue";
 import DataTable from "@/components/ui/DataTable.vue";
 import {vndClient} from "@/api"
-const loading:Ref<boolean> = ref(false),
-    totalPages:Ref<number>=ref(1),
-    pageSize:Ref<number>=ref(10),
-    usersData:Ref<[]>=ref([]),
+import {ISerializedUser} from "../../../../../libs/core";
+
+const loading = ref(false),
+    totalPages=ref(1),
+    pageSize=ref(10),
+    usersData=ref<ISerializedUser[]>([]),
     tableFields=[
         { field: 'firstName', title: 'Name' ,hide: false},
         { field: 'email', title: 'Email' ,hide: false},
         { field: 'phoneNumber', title: 'Phone No.' ,hide: false},
         { field: 'role', title: 'Role' ,hide: false},
         { field: 'createdAt', title: 'Created At' ,hide: false,type: 'date'},
+        {field:'action',title:'Action'}
     ]
 interface pageData {
     currentPage:number,
@@ -38,12 +41,13 @@ const users = async (data:pageData) =>{
     try {
         loading.value=true;
         const users = await vndClient.users.getMany({page:data.currentPage,limit:data.pageSize});
+        // @ts-expect-error - to be fixed by backend
         usersData.value=users.data;
         totalPages.value=users.total;
         pageSize.value = data.pageSize
-        console.log(users)
+
     }catch (err){
-        console.log(err)
+
     }finally {
         loading.value=false;
     }
