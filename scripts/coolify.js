c89fccc218a7106c4e77ba121006ed0efc8c736e
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-05-15 13:40:35 +0300

    fix: POST -> GET
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

--- scripts/coolify.js
+++ scripts/coolify.js
@@ -6,18 +6,22 @@ const { Command } = require('commander');
 const program = new Command();
 program
   .option('-w, --webhook <url>', 'Coolify webhook URL')
-  .option('-t, --token <token>', 'Coolify token')
+  // .option('-t, --token <token>', 'Coolify token')
   .parse(process.argv);
 
 const { webhook, token } = program.opts();
-console.log(webhook, token);
+console.log({ webhook, token });
+
+// webhook has  "http://localhost/=" so we slice it to get the correct url
 
 axios
   .get(webhook, {
     headers: {
-      'Content-Type': 'application/json',
       Authorization: `Bearer 3|r3xqOhamqkN5o7fr1kjelOiMhOoNO5faej7A2LK47b0f07e5`,
     },
+  })
+  .then((response) => {
+    console.log(response);
   })
   .catch((error) => {
     console.error(error);
