60edd17dc12872b042a15c3cba098f3404e83162
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-05-15 13:02:43 +0300

    fix: POST -> GET
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

--- scripts/coolify.js
+++ scripts/coolify.js
@@ -10,12 +10,13 @@ program
   .parse(process.argv);
 
 const { webhook, token } = program.opts();
+console.log(webhook, token);
 
 axios
   .get(webhook, {
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
+      Authorization: `Bearer 3|r3xqOhamqkN5o7fr1kjelOiMhOoNO5faej7A2LK47b0f07e5`,
     },
   })
   .catch((error) => {
