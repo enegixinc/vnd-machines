4a5befeafe896232462cfa91263534e8a47e85f0
Omar Adly <omaradly2020@gmail.com>
2024-05-20 14:46:57 +0300

    refactor: default menu horizontal

--- apps/frontend/theme.config.ts
+++ apps/frontend/theme.config.ts
@@ -2,7 +2,7 @@
 export const $themeConfig = {
     locale: 'en', // en, da, de, el, es, fr, hu, it, ja, pl, pt, ru, sv, tr, zh
     theme: 'system', // light, dark, system
-    menu: 'vertical', // vertical, collapsible-vertical, horizontal
+    menu: 'horizontal', // vertical, collapsible-vertical, horizontal
     layout: 'full', // full, boxed-layout
     rtlClass: 'ltr', // rtl, ltr
     animation: '', // animate__fadeIn, animate__fadeInDown, animate__fadeInUp, animate__fadeInLeft, animate__fadeInRight, animate__slideInDown, animate__slideInLeft, animate__slideInRight, animate__zoomIn
