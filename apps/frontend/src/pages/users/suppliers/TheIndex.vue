05d1528a71563ee3a72cb096e74316978c400493
Omar Adly <omaradly2020@gmail.com>
2024-05-23 22:16:19 +0300

    refactor: suppliers use composables

--- apps/frontend/src/pages/users/suppliers/TheIndex.vue
+++ apps/frontend/src/pages/users/suppliers/TheIndex.vue
@@ -8,61 +8,31 @@
             :fields = "tableFields"
             :loading="loading"
             :table-title="$t('usersPages.suppliers')"
-            @change-server="suppliers"
+            @change-server="fetchSuppliers"
+            @delete-row="deleteUser"
             :sortable="true"
             sort-by="firstName"
         />
     </div>
 </template>
 <script setup lang="ts">
-import {computed, ref} from 'vue';
-import TheBreadcrumbs from "@/components/ui/TheBreadcrumbs.vue";
-import DataTable from "@/components/ui/DataTable.vue";
-import {vndClient} from "@/api"
-import {ISerializedUser} from "@core";
+import {computed} from 'vue';
 import {useI18n} from 'vue-i18n'
-const i18n = useI18n()
-
-const loading = ref(false),
-    totalPages=ref(1),
-    pageSize=ref<number|undefined>(10),
-    usersData=ref<ISerializedUser[]>([]),
-    tableFields=computed(()=>{
+const {t} = useI18n()
+import useUser from "@/composables/users/use-user";
+const {loading,totalPages,usersData,pageSize,TheBreadcrumbs,DataTable,fetchUsers:fetchSuppliers,deleteUser} = useUser({
+    filter:['role||$eq||supplier']
+})
+const tableFields=computed(()=>{
         return [
-            { field: 'firstName', title: i18n.t("fields.name") ,condition:"equal",hide: false},
-            { field: 'email', title: i18n.t("fields.email") ,hide: false},
-            { field: 'phoneNumber', title: i18n.t('fields.phoneNo') ,hide: false},
-            { field: 'businessName', title: i18n.t('fields.businessName') ,hide: false},
-            { field: 'active', title: i18n.t('fields.status') ,hide: false},
+            { field: 'firstName', title: t("fields.name") ,condition:"equal",hide: false},
+            { field: 'email', title: t("fields.email") ,hide: false},
+            { field: 'phoneNumber', title: t('fields.phoneNo') ,hide: false},
+            { field: 'businessName', title: t('fields.businessName') ,hide: false},
+            { field: 'active', title: t('fields.status') ,hide: false},
             {field:'action',title:'',filter:false,sort:false}
         ]
     })
-
-
-// import {$OpenApiTs} from "@frontend/api-sdk"
-
-type requestType = Parameters<typeof vndClient.users.getMany>[0]
-
-const suppliers = async (data: requestType = {}) =>{
-    try {
-        loading.value=true;
-        if (data.filter){
-            data.filter.push('role||$eq||supplier')
-        }else {
-            data.filter = ['role||$eq||supplier']
-        }
-        const suppliers = await vndClient.users.getMany(data);
-        // @ts-expect-error - to be fixed by backend
-        usersData.value=suppliers.data;
-        totalPages.value=suppliers.total;
-        pageSize.value = data?.limit || 10
-
-    }catch (err){
-        console.log(err)
-    }finally {
-        loading.value=false;
-    }
-}
-suppliers({page:1,limit:pageSize.value});
+fetchSuppliers({page:1,limit:pageSize.value});
 
 </script>
