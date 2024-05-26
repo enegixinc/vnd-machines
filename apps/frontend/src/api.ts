e7e070cd529e05e7ea0f62400f38c6c72536985e
Omar Adly <omaradly2020@gmail.com>
2024-05-26 15:02:04 +0300

    feat: env file and read in apis

--- apps/frontend/src/api.ts
+++ apps/frontend/src/api.ts
@@ -1,27 +1,28 @@
 import { VNDClient } from '@frontend/api-sdk';
-// import { type AxiosRequestConfig } from 'axios';
-// import {useUser} from "@/stores/user"
-// const user = useUser();
-// // eslint-disable-next-line no-unused-vars
-// type Middleware<T> = (value: T) => Promise<T> | T;
+import { type AxiosRequestConfig } from 'axios';
+import {useUser} from "@/stores/user"
+
+const user = useUser();
+// eslint-disable-next-line no-unused-vars
+type Middleware<T> = (value: T) => Promise<T> | T;
 export const vndClient = new VNDClient({
-    BASE: 'https://vnd-api.5ostudios.com',
+    BASE: import.meta.env.VITE_APP_BASE_URL,
     HEADERS: {
-        // 'Access-Control-Allow-Origin': '*',
-        // 'Content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Content-type': 'application/json',
     },
 });
 // ----------------- this give core error now
-// const attachHeaders: Middleware<AxiosRequestConfig<unknown>> = async (
-//     request,
-// ) => {
-//     return {
-//         ...request,
-//         headers: {
-//             ...request.headers,
-//             Authorization: `Bearer ${user.token}`,
-//         },
-//     };
-// };
-// vndClient.request.config.interceptors.request.use(attachHeaders);
+const attachHeaders: Middleware<AxiosRequestConfig<unknown>> = async (
+    request,
+) => {
+    return {
+        ...request,
+        headers: {
+            ...request.headers,
+            Authorization: `Bearer ${user.token}`,
+        },
+    };
+};
+vndClient.request.config.interceptors.request.use(attachHeaders);
 //
