fd4babb4f381699dbb14e229a74f92af2bb13087
Omar Adly <omaradly2020@gmail.com>
2024-05-28 14:23:12 +0300

    feat: composable use-brands

--- apps/frontend/src/composables/brands/use-brands.ts
+++ apps/frontend/src/composables/brands/use-brands.ts
@@ -1,7 +1,7 @@
-import { ISerializedCategory } from '@core';
+import { ISerializedBrand } from '@core';
 import { vndClient } from '@/api';
 import useEntityFactory from '@/composables/entityFactory/useEntityFactory';
 
-type CategoriesRequestParams = Parameters<typeof vndClient.categories.getMany>[0];
+type BrandsRequestParams = Parameters<typeof vndClient.brands.getMany>[0];
 
-export const useCategories = (defaultSettings: CategoriesRequestParams) => useEntityFactory<ISerializedCategory, CategoriesRequestParams>(vndClient.categories)(defaultSettings);
+export const useBrands = (defaultSettings: BrandsRequestParams) => useEntityFactory<ISerializedBrand, BrandsRequestParams>(vndClient.brands)(defaultSettings);
