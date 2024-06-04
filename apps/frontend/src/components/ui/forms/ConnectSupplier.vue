80d17cf34ce89a740a7aea28857c5380524a0653
Omar Adly <omaradly2020@gmail.com>
2024-06-04 19:37:43 +0300

    feat: get supplier by name,email,phone,businessName

--- apps/frontend/src/components/ui/forms/ConnectSupplier.vue
+++ apps/frontend/src/components/ui/forms/ConnectSupplier.vue
@@ -1,6 +1,6 @@
 <template>
-    <div :class="{'has-error':errorMessage,'has-success':meta.dirty && meta.valid}">
-        <label :for="name" >{{$t('fields.supplier')}}<span v-if="requierd" class="text-danger mx-1">*</span></label>
+    <div :class="{ 'has-error': errorMessage, 'has-success': meta.dirty && meta.valid }">
+        <label :for="name">{{ $t('fields.supplier') }}<span v-if="requierd" class="text-danger mx-1">*</span></label>
         <multiselect
             v-model="selectedSupplier"
             :options="Suppliers"
@@ -16,13 +16,15 @@
             track-by="_id"
             :loading="loading"
         >
-            <template #option="{option}">
-                <h2>{{option.firstName}} {{option.lastName}} <span class="text-[12px]">&#91; {{option.businessName}} &#93;</span></h2>
-                <span class="block text-[12px]">{{option.email}}</span>
-                <bdi class="block text-[12px]">{{option.phoneNumber}}</bdi>
+            <template #option="{ option }">
+                <h2>
+                    {{ option.firstName }} {{ option.lastName }} <span class="text-[12px]">&#91; {{ option.businessName }} &#93;</span>
+                </h2>
+                <span class="block text-[12px]">{{ option.email }}</span>
+                <bdi class="block text-[12px]">{{ option.phoneNumber }}</bdi>
             </template>
-            <template #singleLabel="{option}">
-                {{option.firstName}} {{option.lastName}} <span class="text-[12px]">&#91; {{option.businessName}} &#93;</span>
+            <template #singleLabel="{ option }">
+                {{ option.firstName }} {{ option.lastName }} <span class="text-[12px]">&#91; {{ option.businessName }} &#93;</span>
             </template>
             <template #noResult>
                 {{ $t('noResults') }}
@@ -33,43 +35,54 @@
         </multiselect>
         <div class="min-h-[32px] py-[8px] min-w[1px] overflow-hidden">
             <Transition name="error" mode="out-in">
-                              <span v-if="errorMessage" class="text-danger block font-normal text-[12px] leading-[16px] break-all text-wrap hyphens-auto">
-                                {{ errorMessage }}
-                              </span>
+                <span v-if="errorMessage" class="text-danger block font-normal text-[12px] leading-[16px] break-all text-wrap hyphens-auto">
+                    {{ errorMessage }}
+                </span>
             </Transition>
         </div>
     </div>
 </template>
 <script lang="ts" setup>
-import {ref, watch} from 'vue';
-import Multiselect from '@suadelabs/vue3-multiselect';
-import '@suadelabs/vue3-multiselect/dist/vue3-multiselect.css';
-import {useUser} from '@/composables/users/use-user2'
+    import { ref, watch } from 'vue';
+    import Multiselect from '@suadelabs/vue3-multiselect';
+    import '@suadelabs/vue3-multiselect/dist/vue3-multiselect.css';
+    import { useUser } from '@/composables/users/use-user2';
+    import { useField } from 'vee-validate';
 
-const selectedSupplier = ref();
-interface props{
-    name:string,
-    requierd?: boolean;
-
-}
-const data = defineProps<props>()
-import { useField } from 'vee-validate';
-const { errorMessage, meta,resetField } = useField(() => data.name,undefined,{
-
-});
-const {loading,fetchEntities:fetchSuppliers,entityData:Suppliers} = useUser({
-    filter:['role||$eq||supplier']
-})
-const getSuppliers = async (searchKey:string)=>{
-    if (searchKey.trim().length){
-        await fetchSuppliers({page:1,limit:100,sort: ['firstName,DESC'],filter:[`firstName||$cont||${searchKey}`]})
+    const selectedSupplier = ref();
+    interface props {
+        name: string;
+        requierd?: boolean;
     }
-}
-const setValue = (newVal)=>{
-    resetField({
-        value:newVal?._id || ''
-    })
-}
-watch(selectedSupplier,setValue)
-
+    const data = defineProps<props>();
+    const { errorMessage, meta, resetField } = useField(() => data.name, undefined, {});
+    const {
+        loading,
+        fetchEntities: fetchSuppliers,
+        entityData: Suppliers,
+    } = useUser({
+        filter: ['role||$eq||supplier'],
+    });
+    const getSuppliers = async (searchKey: string) => {
+        if (searchKey.trim().length) {
+            await fetchSuppliers({
+                page: 1,
+                limit: 100,
+                sort: ['firstName,DESC'],
+                or: [
+                    `firstName||$contL||${searchKey}&filter=role||$eq||supplier`,
+                    `lastName||$contL||${searchKey}&filter=role||$eq||supplier`,
+                    `email||$contL||${searchKey}&filter=role||$eq||supplier`,
+                    `phoneNumber||$contL||${searchKey}&filter=role||$eq||supplier`,
+                    `businessName||$contL||${searchKey}&filter=role||$eq||supplier`,
+                ],
+            });
+        }
+    };
+    const setValue = (newVal) => {
+        resetField({
+            value: newVal?._id || '',
+        });
+    };
+    watch(selectedSupplier, setValue);
 </script>
