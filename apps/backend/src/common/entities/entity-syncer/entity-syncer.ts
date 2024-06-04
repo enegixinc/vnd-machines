b46cc8b8902842972d21fd420e9cf6b9faff722e
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-06-04 04:37:04 +0300

    refactor: entity sync await
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

--- apps/backend/src/common/entities/entity-syncer/entity-syncer.ts
+++ apps/backend/src/common/entities/entity-syncer/entity-syncer.ts
@@ -9,7 +9,7 @@ import { CRUDSyncer } from './crud-syncer';
 import { _IMagex_DatabaseEntity } from '@core';
 
 export interface EntitySyncer<Entity> {
-  handleRelationships(record: unknown): Entity;
+  handleRelationships(record: unknown): Entity | Promise<Entity>;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
@@ -20,28 +20,32 @@ export abstract class EntitySyncer<
   extends CRUDSyncer<Entity>
   implements EntitySubscriberInterface<Entity>, OnModuleInit
 {
+  protected records: Entity[] = [];
+  protected magexRecords: MagexRecord[] = [];
+
   protected constructor(
-    @Inject(DataSource) protected dataSource: DataSource,
-    @Inject(MagexService) protected magexService: MagexService
+    @Inject(DataSource) protected readonly dataSource: DataSource,
+    @Inject(MagexService) protected readonly magexService: MagexService
   ) {
     super(magexService);
     this.dataSource.subscribers.push(this);
   }
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  private get Entity(): string | Function {
-    return this.listenTo();
-  }
-  private records: Entity[] = [];
-  protected magexRecords: MagexRecord[] = [];
-  protected get entityLookupTable() {
-    return new Map(this.records.map((record) => [record._id, record]));
-  }
-  private get EntityClone(): MagexDatabaseEntity {
-    return this.dataSource.manager.create(this.Entity);
-  }
 
   // eslint-disable-next-line @typescript-eslint/ban-types
   abstract listenTo(): string | Function;
+
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  private get entity(): string | Function {
+    return this.listenTo();
+  }
+
+  private get entityClone(): MagexDatabaseEntity {
+    return this.dataSource.manager.create(this.entity);
+  }
+
+  protected get entityLookupTable() {
+    return new Map(this.records.map((record) => [record._id, record]));
+  }
 
   async onModuleInit() {
     await this.syncWithMagex();
@@ -49,12 +53,13 @@ export abstract class EntitySyncer<
 
   @timer()
   async fetchOurRecords() {
-    this.records = await this.dataSource.manager.find(this.listenTo());
+    this.records = await this.dataSource.manager.find(this.entity);
   }
+
   async fetchMagexRecords() {
-    console.log('fetching magex records', this.EntityClone);
+    console.log('fetching magex records', this.entityClone);
     // @ts-expect-error - TODO: fix this
-    this.magexRecords = await this.EntityClone.fetchMagexRecords(
+    this.magexRecords = await this.entityClone.fetchMagexRecords(
       this.magexService
     );
   }
@@ -64,13 +69,16 @@ export abstract class EntitySyncer<
     const newRecords = new Set<MagexRecord>();
     const updatedRecords = new Set<MagexRecord>();
 
-    this.magexRecords.filter((magexRecord) => {
+    this.magexRecords.forEach((magexRecord) => {
       const record = this.entityLookupTable.get(magexRecord._id);
       const gotUpdated =
         record && new Date(record.lastSyncAt) < new Date(magexRecord.updatedAt);
 
-      if (!record) newRecords.add(magexRecord);
-      else if (gotUpdated) updatedRecords.add(magexRecord);
+      if (!record) {
+        newRecords.add(magexRecord);
+      } else if (gotUpdated) {
+        updatedRecords.add(magexRecord);
+      }
     });
 
     console.log('new records:', newRecords.size);
@@ -83,20 +91,25 @@ export abstract class EntitySyncer<
   }
 
   @timer()
-  prepareRecords(records: unknown[]) {
-    return records.map((record) => {
-      const entity = this.EntityClone;
-      Object.assign(entity, this.assignRelations(record));
-      Object.assign(entity, { lastSyncAt: new Date().toISOString() });
-      return entity;
-    });
+  async prepareRecords(records: MagexRecord[]) {
+    return Promise.all(
+      records.map(async (record) => {
+        const entity = this.entityClone;
+        Object.assign(entity, await this.assignRelations(record));
+        Object.assign(entity, { lastSyncAt: new Date().toISOString() });
+        return entity;
+      })
+    );
   }
-  private assignRelations(record: unknown) {
-    return this.handleRelationships ? this.handleRelationships(record) : record;
+
+  private async assignRelations(record: unknown) {
+    return this.handleRelationships
+      ? await this.handleRelationships(record)
+      : record;
   }
 
   @timer()
-  private async saveRecords(records: unknown[]) {
+  private async saveRecords(records: MagexDatabaseEntity[]) {
     await this.dataSource.manager.save(records, {
       listeners: false,
       chunk: 1000,
@@ -111,15 +124,18 @@ export abstract class EntitySyncer<
   @timer()
   async syncWithMagex() {
     // @ts-expect-error - TODO: add type
-    console.log('Syncing with Magex', this.Entity.name);
+    console.log('Syncing with Magex', this.entity.name);
     await Promise.all([this.fetchOurRecords(), this.fetchMagexRecords()]);
 
     const { newRecords, updatedRecords } = this.identifyOutOfSyncRecords();
-    const records = this.prepareRecords([...newRecords, ...updatedRecords]);
+    const preparedRecords = await this.prepareRecords([
+      ...newRecords,
+      ...updatedRecords,
+    ]);
 
-    await this.saveRecords(records);
+    await this.saveRecords(preparedRecords);
 
-    // clear all records even soft deleted ones
-    // await this.dataSource.manager.delete(this.listenTo(), {});
+    // Uncomment this if you want to delete all records including soft deleted ones
+    // await this.dataSource.manager.delete(this.entity, {});
   }
 }
