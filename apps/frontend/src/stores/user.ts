be5719049622b16194c7fe7fd976a3b29f44cd95
Omar Adly <omaradly2020@gmail.com>
2024-06-07 19:22:36 +0300

    fix: stop timer

--- apps/frontend/src/stores/user.ts
+++ apps/frontend/src/stores/user.ts
@@ -1,60 +1,70 @@
-import { defineStore } from 'pinia'
-import { ref,computed } from 'vue'
+import { defineStore } from 'pinia';
+import { computed, ref } from 'vue';
 
 export const useUser = defineStore('user', () => {
     const user = ref<unknown>(null),
         token = ref<string | null>(null),
         refreshToken = ref<string | null>(null),
-        didAutoLogout=ref(false),
-        expiredDate =ref(0),
-        isAuthenticated = computed(()=>{
-            return !!token.value
-        });//
-    let timer:number;
+        didAutoLogout = ref(false),
+        expiredDate = ref(0),
+        isAuthenticated = computed(() => {
+            return !!token.value;
+        }); //
+    let timer: number;
 
-
-    function setUser(userInfo:unknown, userToken:string|null,userRefreshToken:string|null, expiresIn:number,newLogin?:boolean, rememberMe?:boolean):void{
-        user.value= userInfo;
+    function setUser(
+        userInfo: unknown,
+        userToken: string | null,
+        userRefreshToken: string | null,
+        expiresIn: number,
+        newLogin?: boolean,
+        rememberMe?: boolean
+    ): void {
+        user.value = userInfo;
         token.value = userToken;
-        refreshToken.value= userRefreshToken;
+        refreshToken.value = userRefreshToken;
         expiredDate.value = expiresIn;
-        if (newLogin){
-            expiresIn = expiresIn? expiresIn * 1000 : 0;
+
+        if (newLogin) {
+            expiresIn = expiresIn ? expiresIn * 1000 : 0;
+
             const expirationDate = new Date(expiresIn).getTime();
-            if (userRefreshToken && userToken && userInfo && expirationDate && rememberMe){
+            const expiresInSeconds = expirationDate / 1000;
+
+            if (userRefreshToken && userToken && userInfo && expirationDate && rememberMe) {
                 localStorage.setItem('token', userToken);
                 localStorage.setItem('refreshToken', userRefreshToken);
                 localStorage.setItem('user', JSON.stringify(userInfo));
                 localStorage.setItem('tokenExpiration', expirationDate.toString());
             }
-            timer = setTimeout(function() {
-                autoLogout();
-            }, expiresIn);
-            expiredDate.value =  expirationDate;
+            // timer = setTimeout(function () {
+            //     console.log('i run');
+            //     autoLogout();
+            // }, expiresInSeconds * 1000);
+            expiredDate.value = expirationDate;
         }
     }
-    function setAutoLogout(){
+    function setAutoLogout() {
         didAutoLogout.value = true;
     }
-    function  tryLogin() {
-
+    function tryLogin() {
         const token = localStorage.getItem('token');
         const user = localStorage.getItem('user');
         const tokenExpiration = localStorage.getItem('tokenExpiration');
         const refreshToken = localStorage.getItem('refreshToken');
 
-        const expiresIn = tokenExpiration && !isNaN(+tokenExpiration)? +tokenExpiration - new Date().getTime():0;
+        const expiresIn = tokenExpiration && !isNaN(+tokenExpiration) ? +tokenExpiration - new Date().getTime() : 0;
 
         if (expiresIn <= 0) {
             return;
         }
         // bescause number in first too big
-        const expiresInSeconds = expiresIn / 1000;
-        timer = setTimeout(function() {
-            console.log(expiresIn)
-        }, expiresInSeconds * 1000);
+        // const expiresInSeconds = expiresIn / 1000;
+        // timer = setTimeout(function () {
+        //     console.log(expiresIn);
+        // }, expiresInSeconds * 1000);
         if (token && user && tokenExpiration) {
-            setUser(user,token,refreshToken,+tokenExpiration)
+            setUser(user, token, refreshToken, +tokenExpiration);
         }
     }
 
@@ -64,13 +74,12 @@ export const useUser = defineStore('user', () => {
         localStorage.removeItem('tokenExpiration');
         localStorage.removeItem('refreshToken');
         clearTimeout(timer);
-        setUser(null,null,null,0)
+        setUser(null, null, null, 0);
     }
-   function autoLogout() {
+    function autoLogout() {
         logout();
         setAutoLogout();
     }
-
 
     return {
         user,
@@ -81,7 +90,6 @@ export const useUser = defineStore('user', () => {
         setAutoLogout,
         logout,
         tryLogin,
-        refreshToken
-    }
-})
-
+        refreshToken,
+    };
+});
