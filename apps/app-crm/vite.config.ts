2f635d726d16ac00a4013eca4d888218e631122e
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-06-10 02:18:21 +0300

    feat: new app2
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

---
+++ apps/app-crm/vite.config.ts
@@ -0,0 +1,24 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [tsconfigPaths({ root: __dirname }), react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          antd: ['antd'],
+        },
+      },
+    },
+  },
+});
+
+// with nx
+// import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import tsconfigPaths from 'vite-tsconfig-paths';
+// import { join } from 'path';
+//
