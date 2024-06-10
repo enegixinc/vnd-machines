e80819d24aaf36c1d28a03d520f0cd00b9141cf3
Omar Adly <omaradly2020@gmail.com>
2024-06-10 14:02:37 +0300

    feat:show and update brand types

--- apps/frontend/src/composables/brands/use-brands.ts
+++ apps/frontend/src/composables/brands/use-brands.ts
@@ -3,5 +3,6 @@ import { vndClient } from '@/api';
 import useEntityFactory from '@/composables/entityFactory/useEntityFactory';
 
 type BrandsRequestParams = Parameters<typeof vndClient.brands.getMany>[0];
-
-export const useBrands = (defaultSettings: BrandsRequestParams) => useEntityFactory<ISerializedBrand, BrandsRequestParams>(vndClient.brands)(defaultSettings);
+type BrandsGetOneParams = Parameters<typeof vndClient.brands.getOne>[0];
+type BrandsUpdateOneParams = Parameters<typeof vndClient.brands.updateOne>[0];
+export const useBrands = (defaultSettings: BrandsRequestParams) => useEntityFactory<ISerializedBrand, BrandsRequestParams,BrandsGetOneParams,BrandsUpdateOneParams>(vndClient.brands)(defaultSettings);
