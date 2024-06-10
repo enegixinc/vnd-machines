d6443b42b997178bf6eb16f621faf09b57db234f
Omar Adly <omaradly2020@gmail.com>
2024-06-10 15:06:46 +0300

    feat:show,upadte types categories composables

--- apps/frontend/src/composables/categories/use-categories.ts
+++ apps/frontend/src/composables/categories/use-categories.ts
@@ -3,5 +3,6 @@ import { vndClient } from '@/api';
 import useEntityFactory from '@/composables/entityFactory/useEntityFactory';
 
 type CategoriesRequestParams = Parameters<typeof vndClient.categories.getMany>[0];
-
-export const useCategories = (defaultSettings: CategoriesRequestParams) => useEntityFactory<ISerializedCategory, CategoriesRequestParams>(vndClient.categories)(defaultSettings);
+type CategoriesGetOneParams = Parameters<typeof vndClient.categories.getOne>[0];
+type CategoriesUpdateOneParams = Parameters<typeof vndClient.categories.updateOne>[0];
+export const useCategories = (defaultSettings: CategoriesRequestParams) => useEntityFactory<ISerializedCategory, CategoriesRequestParams,CategoriesGetOneParams,CategoriesUpdateOneParams>(vndClient.categories)(defaultSettings);
