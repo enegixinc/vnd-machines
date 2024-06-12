e613cdfbf973bbbcabde779ead807934fa301b2b
Omar Adly <omaradly2020@gmail.com>
2024-06-12 11:33:34 +0300

    feat:fill machine links

--- apps/frontend/src/components/layout/Sidebar.vue
+++ apps/frontend/src/components/layout/Sidebar.vue
@@ -173,7 +173,7 @@
                                         <router-link :to="{name:'manageMachines'}" @click="toggleMobileMenu">{{ $t('links.manageMachines') }}</router-link>
                                     </li>
                                     <li>
-                                        <router-link to="/datatables/advanced" @click="toggleMobileMenu">{{ $t('links.fillMachine') }}</router-link>
+                                        <router-link :to="{name:'fillingMachines'}" @click="toggleMobileMenu">{{ $t('links.fillMachine') }}</router-link>
                                     </li>
                                 </ul>
                             </vue-collapsible>
--- apps/frontend/src/router/machines.ts
+++ apps/frontend/src/router/machines.ts
@@ -6,6 +6,11 @@ export const machinesLinks: RouteRecordRaw[] = [
         name: 'manageMachines',
         component: () => import('@/pages/machines/TheIndex.vue'),
     },
+    {
+        path: '/machines/fill',
+        name: 'fillingMachines',
+        component: () => import('@/pages/machines/fillMachine/TheIndex.vue')
+    },
     // {
     //     path: '/machines/:id',
     //     props: true,
